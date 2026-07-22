153902509c5a967ea040812de1ab3b5fed2831d2
s1080313 <103295375+s1080313@users.noreply.github.com>
2026-07-22 10:12:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-def divide(a, b):
-    if b == 0:
-        raise ValueError("不能除以零")
-    return a / b
+API_KEY = "sk-test-1234567890"
 
+def divide(a, b): return a / b
 
-if __name__ == "__main__":
-    print(divide(10, 2))
+if name == "main": print(divide(10, 2)) print(divide(10, 0))
