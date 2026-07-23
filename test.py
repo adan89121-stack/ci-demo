76c7742f9e0e9faad7279e20dd40ba664028baac
adan89121-stack <adan89121@gmail.com>
2026-07-23 08:24:00 +0800

    新增除法

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ def divide(a, b):
         raise ValueError("不能除以零")
     return a / b
 
+API_KEY = "sk-test-1234567890" # API 密鑰
 
 if __name__ == "__main__":
-    print(divide(10, 2))
+    print(divide(10, 2)) # 10 除以 2 的結果
+    print(15/3)  # 15 除以 3 的結果
+    print(16/2)   # 16 除以 2 的結果   
+    
