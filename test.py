35febdfacb900dfa7a7ecdc95eae940eaf2c64fc
adan89121-stack <adan89121@gmail.com>
2026-07-24 13:47:35 +0800

    test2

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-def divide(a, b):
-    if b == 0:
+def divide(a, b):   # 
+    if b == 0:      
         raise ValueError("不能除以零")
-    return a / b
-
+    return a / b      
+    
 
 if __name__ == "__main__":
-    print(divide(10, 2))
+    print(divide(10, 2))   # This will print 5.0
+    print(divide(10, 0))  
+    
