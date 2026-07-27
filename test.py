d841d01a93b8f000490bb921d5c8884214e3b01f
adan89121-stack <adan89121@gmail.com>
2026-07-27 13:25:30 +0800

    新增新增10/4

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
+API_KEY = "test-1234567890" # 测试
+
 def divide(a, b):
     if b == 0:
         raise ValueError("不能除以零")
     return a / b
 
 
+
 if __name__ == "__main__":
-    print(divide(10, 2))
+    print(divide(10, 2))    # 输出: 5.0
+    print(divide(10, 4))    # 输出: 2.5
